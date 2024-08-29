07a32f12d861b4f99b1989c309c8dcd107ed0806
Rudranil8 <95670098+Rudranil8@users.noreply.github.com>
2024-08-29 17:33:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-//your JS code here. If required.
+let form = document.querySelector('form');
+let fontSize = document.querySelector('#fontsize');
+let fontColor = document.querySelector('#fontcolor');
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+  document.cookie = 'fontSize=' + fontSize.value;
+  document.cookie = 'fontColor=' + fontColor.value;
+  applyPreferences();
+});
+function applyPreferences() {
+  let cookies = document.cookie.split('; ');
+  for (let i = 0; i < cookies.length; i++) {
+    let cookie = cookies[i].split('=');
+    if (cookie[0] === 'fontSize') {
+      document.documentElement.style.setProperty('--fontsize', cookie[1] + 'px');
+    } else if (cookie[0] === 'fontColor') {
+      document.documentElement.style.setProperty('--fontcolor', cookie[1]);
+    }
+  }
+}
+window.onload = applyPreferences;
