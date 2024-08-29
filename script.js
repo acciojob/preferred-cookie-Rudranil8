19eafc5615413ad521593506c43373ed94c3c701
Rudranil8 <95670098+Rudranil8@users.noreply.github.com>
2024-08-29 20:29:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,36 @@
+// let form = document.querySelector('form');
+// let fontSize = document.querySelector('#fontsize');
+// let fontColor = document.querySelector('#fontcolor');
+// form.addEventListener('submit', function(event) {
+//   event.preventDefault();
+//   document.cookie = 'fontSize=' + fontSize.value;
+//   document.cookie = 'fontColor=' + fontColor.value;
+//   applyPreferences();
+// });
+// function applyPreferences() {
+//   let cookies = document.cookie.split('; ');
+//   for (let i = 0; i < cookies.length; i++) {
+//     let cookie = cookies[i].split('=');
+//     if (cookie[0] === 'fontSize') {
+//       document.documentElement.style.setProperty('--fontsize', cookie[1] + 'px');
+//     } else if (cookie[0] === 'fontColor') {
+//       document.documentElement.style.setProperty('--fontcolor', cookie[1]);
+//     }
+//   }
+// }
+// window.onload = applyPreferences;
+
 let form = document.querySelector('form');
 let fontSize = document.querySelector('#fontsize');
 let fontColor = document.querySelector('#fontcolor');
+
 form.addEventListener('submit', function(event) {
   event.preventDefault();
-  document.cookie = 'fontSize=' + fontSize.value;
-  document.cookie = 'fontColor=' + fontColor.value;
+  document.cookie = 'fontSize=' + fontSize.value + '; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/';
+  document.cookie = 'fontColor=' + fontColor.value + '; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/';
   applyPreferences();
 });
+
 function applyPreferences() {
   let cookies = document.cookie.split('; ');
   for (let i = 0; i < cookies.length; i++) {
@@ -18,4 +42,5 @@ function applyPreferences() {
     }
   }
 }
+
 window.onload = applyPreferences;
