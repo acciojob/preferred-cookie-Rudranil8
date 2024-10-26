3b39dd972ab4f71181c2b918b4ec35de3c9d2788
Rudranil8 <95670098+Rudranil8@users.noreply.github.com>
2024-10-26 21:19:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,44 @@
 // let form = document.querySelector('form');
 // let fontSize = document.querySelector('#fontsize');
 // let fontColor = document.querySelector('#fontcolor');
+
 // form.addEventListener('submit', function(event) {
 //   event.preventDefault();
-//   document.cookie = 'fontSize=' + fontSize.value;
-//   document.cookie = 'fontColor=' + fontColor.value;
+//   document.cookie = 'fontSize=' + fontSize.value + '; expires=Fri, 31 Dec 9999 23:59:59 GMT';
+//   document.cookie = 'fontColor=' + fontColor.value + '; expires=Fri, 31 Dec 9999 23:59:59 GMT';
 //   applyPreferences();
 // });
+// // function applyPreferences() {
+// //   for (let c of document.cookie.split('; ')) {
+// //     let [key, val] = c.split('=');
+// //     if (key === 'fontSize') {
+// //       document.documentElement.style.setProperty('--fontsize', val + 'px');
+// //     } else if (key === 'fontColor') {
+// //       document.documentElement.style.setProperty('--fontcolor', val);
+// //     }
+// //   }
+// // }
 // function applyPreferences() {
-//   let cookies = document.cookie.split('; ');
-//   for (let i = 0; i < cookies.length; i++) {
-//     let cookie = cookies[i].split('=');
-//     if (cookie[0] === 'fontSize') {
-//       document.documentElement.style.setProperty('--fontsize', cookie[1] + 'px');
-//     } else if (cookie[0] === 'fontColor') {
-//       document.documentElement.style.setProperty('--fontcolor', cookie[1]);
+//   return new Promise(resolve => {
+//     for (let c of document.cookie.split('; ')) {
+//       let [key, val] = c.split('=');
+//       if (key === 'fontSize') {
+//         document.documentElement.style.setProperty('--fontsize', val + 'px');
+//       } else if (key === 'fontColor') {
+//         document.documentElement.style.setProperty('--fontcolor', val);
+//       }
 //     }
-//   }
+//     resolve();
+//   });
 // }
-// window.onload = applyPreferences;
+
+// window.onload = () => applyPreferences();
+
+// // window.onload = applyPreferences;
+// cy.window().then(win => win.applyPreferences()).then(() => {
+//   cy.get("body").should("have.css", "font-size", "18px");
+//   cy.get("body").should("have.css", "color", "rgb(255, 0, 0)");
+// });
 
 let form = document.querySelector('form');
 let fontSize = document.querySelector('#fontsize');
@@ -30,34 +50,16 @@ form.addEventListener('submit', function(event) {
   document.cookie = 'fontColor=' + fontColor.value + '; expires=Fri, 31 Dec 9999 23:59:59 GMT';
   applyPreferences();
 });
-// function applyPreferences() {
-//   for (let c of document.cookie.split('; ')) {
-//     let [key, val] = c.split('=');
-//     if (key === 'fontSize') {
-//       document.documentElement.style.setProperty('--fontsize', val + 'px');
-//     } else if (key === 'fontColor') {
-//       document.documentElement.style.setProperty('--fontcolor', val);
-//     }
-//   }
-// }
+
 function applyPreferences() {
-  return new Promise(resolve => {
-    for (let c of document.cookie.split('; ')) {
-      let [key, val] = c.split('=');
-      if (key === 'fontSize') {
-        document.documentElement.style.setProperty('--fontsize', val + 'px');
-      } else if (key === 'fontColor') {
-        document.documentElement.style.setProperty('--fontcolor', val);
-      }
+  for (let c of document.cookie.split('; ')) {
+    let [key, val] = c.split('=');
+    if (key === 'fontSize') {
+      document.documentElement.style.setProperty('--fontsize', val + 'px');
+    } else if (key === 'fontColor') {
+      document.documentElement.style.setProperty('--fontcolor', val);
     }
-    resolve();
-  });
+  }
 }
 
-window.onload = () => applyPreferences();
-
-// window.onload = applyPreferences;
-cy.window().then(win => win.applyPreferences()).then(() => {
-  cy.get("body").should("have.css", "font-size", "18px");
-  cy.get("body").should("have.css", "color", "rgb(255, 0, 0)");
-});
+window.onload = applyPreferences;
